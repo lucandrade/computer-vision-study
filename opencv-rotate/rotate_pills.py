8456bdc39dc45ee26d38afd7c0d9b0fd212b26ed
Lucas Andrade <lucas.andrade.oliveira@hotmail.com>
2019-01-09 16:40:25 +0000

    hardcoding the image

--- opencv-rotate/rotate_pills.py
+++ opencv-rotate/rotate_pills.py
@@ -3,19 +3,12 @@
 
 # import the necessary packages
 import numpy as np
-import argparse
 import imutils
 import cv2
 
-# construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required=True,
-	help="path to the image file")
-args = vars(ap.parse_args())
-
 # load the image from disk, convert it to grayscale, blur it,
 # and apply edge detection to reveal the outline of the pill
-image = cv2.imread(args["image"])
+image = cv2.imread("images/pill_01.png")
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 gray = cv2.GaussianBlur(gray, (3, 3), 0)
 edged = cv2.Canny(gray, 20, 100)
